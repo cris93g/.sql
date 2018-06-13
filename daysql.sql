202d5b735f3c98f7827a1631a38c5b76cbbe5c58
cris93g <37388115+cris93g@users.noreply.github.com>
2018-06-13 13:50:16 -0500

    nogreg

--- daysql.sql
+++ daysql.sql
@@ -55,3 +55,4 @@ name)values("john"),("mike"),("brian");
 select * from Artist order by Name desc  limit 10; 
 select * from Artist order by Name asc limit 5;
 select * from Artist where Name like "Black%";
+select * from Artist where Name like "%Black%"
